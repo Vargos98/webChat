46137bab89495481a89a170ce3960c4404f50ac7
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2025-01-09 13:16:33 +0530

    Created the backend logic to connect the webcam of client and user

---
+++ public/js/script.js
@@ -0,0 +1,45 @@
+const socket = io();
+let local;
+let remote;
+let peerConnection;
+
+const rtcSettings = {
+  iceServers : [{urls: "stun:stun.l.google.com:19302"}],
+}
+
+const initialize = async () => {
+  local = await navigator.mediaDevices.getUserMedia({ audio: true, video: true })
+  initiateOffer();
+}
+const initiateOffer = async () => {
+   await createPeerConnection();
+ const offer =  await peerConnection.createOffer();
+ await peerConnection.setLocalDescription(offer)
+ socket.emit("signalingMessage", JSON.stringify({type:"offer",offer}));
+}
+
+const createPeerConnection = async () => {
+ peerConnection = new RTCPeerConnection(rtcSettings)
+
+ remote =new MediaStream();
+  document.querySelector("#remoteVideo").srcObject= remote;
+  document.querySelector("#remoteVideo").style.display="block";
+  document.querySelector("#localVideo").classList.add("smallFrame");
+
+  local.getTracks().forEach(track =>{
+    peerConnection.addTrack(track, local);
+  });
+  peerConnection.ontrack = event => {
+    event.streams[0].getTracks().forEach((track)=>{
+      remote.addTrack(track)
+    })
+    peerConnection.onicecandidate = (event) =>{
+      event.candidate && socket.emit(
+        "signalingMessage", 
+        JSON.stringify(
+          {type:"candidate", candidate:event.candidate}
+        ));
+    }
+  };
+};
+initialize();
