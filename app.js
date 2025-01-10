1762fc1c58f5b62757e7a3ba3cfd72f5b310c643
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2025-01-10 17:22:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server is up on port ${PORT}`);
 });
